5b7cabdc6523328712c99a74421bf590c6c996d9
Rashmi Rajapaksha <110500099+rash933@users.noreply.github.com>
2024-03-13 17:50:26 +0530

    add initial project
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.2.3&packaging=jar&jvmVersion=17&groupId=com.rabittMQ&artifactId=RabbitMQWithSpringboot&name=RabbitMQWithSpringboot&description=Demo%20project%20for%20Spring%20Boot&packageName=com.rabittMQ.RabbitMQWithSpringboot&dependencies=amqp,web,lombok

---
+++ src/main/java/com/rabittMQ/RabbitMQWithSpringboot/RabbitMqWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rabittMQ.RabbitMQWithSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqWithSpringbootApplication.class, args);
+	}
+
+}
